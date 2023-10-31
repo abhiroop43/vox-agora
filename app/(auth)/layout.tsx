3041fb6428d1bad298fc27ce4e590a5fe8f0ff68
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-10-31 20:13:29 +0400

    created folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from 'react';
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return <div className="bg-red-500 h-full">{children}</div>;
+};
+
+export default AuthLayout;
