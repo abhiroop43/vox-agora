6d204c96e691921adf85eb5351e18b7f5641d321
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2023-11-01 17:48:31 +0400

    added clerk authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-  return <div>Protected Route</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -11,8 +12,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={montserrat.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={montserrat.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
