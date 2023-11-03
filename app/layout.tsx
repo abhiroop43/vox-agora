17a2f69a7ac61a31ca9a9f7384ca6d033dc8cab6
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-03 14:43:42 +0400

    added theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { cn } from '@/lib/utils';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -13,8 +15,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={montserrat.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(montserrat.className, 'bg-white dark:bg-[#313338]')}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem storageKey="voxagora-theme">
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
